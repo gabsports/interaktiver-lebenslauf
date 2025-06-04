bf04aa72f76cb17027f8065f9710f3c8a04eca2b
gabsports <134692711+gabsports@users.noreply.github.com>
2025-06-04 10:05:34 +0200

    Remove orbiting buttons and add top button bar

--- src/components/ThreeScene.tsx
+++ src/components/ThreeScene.tsx
@@ -2,26 +2,26 @@ import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import AnimatedObject from './AnimatedObject';
 import { EffectComposer, Bloom } from '@react-three/postprocessing';
-import OrbitingButtons from './OrbitingButtons';
 import Starfield from './Starfield';
+import TopButtonBar from './TopButtonBar';
 
 export default function ThreeScene() {
   return (
-    <div className="flex justify-center items-center w-[400px] h-[400px]">
+    <div className="relative flex justify-center items-center w-[400px] h-[400px]">
+      <TopButtonBar />
 
       <Canvas
-       className="w-[600px] h-[600px] rounded-lg shadow-lg"
-  style={{ width: 1600, height: 1000, background: '#0f0f0f' }}
-  camera={{ position: [0, 0, 6], fov: 50 }}
-  onCreated={({ gl }) => gl.setClearColor('#0f0f0f')}
->
+        className="w-[600px] h-[600px] rounded-lg shadow-lg"
+        style={{ width: 1600, height: 1000, background: '#0f0f0f' }}
+        camera={{ position: [0, 0, 6], fov: 50 }}
+        onCreated={({ gl }) => gl.setClearColor('#0f0f0f')}
+      >
         <ambientLight intensity={0.5} />
         <directionalLight position={[5, 5, 5]} />
 
         <Starfield count={6000} radius={100} />
 
         <AnimatedObject />
-        <OrbitingButtons />
 
         <OrbitControls />
         <EffectComposer>
